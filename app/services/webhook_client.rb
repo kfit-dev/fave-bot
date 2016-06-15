790507d229402c119a61c680dc82a3b514cd62d9
peiyee <tehpeiyee_1989@hotmail.com>
2016-06-15 23:20:03 +0800

    wip

--- app/services/webhook_client.rb
+++ app/services/webhook_client.rb
@@ -5,8 +5,33 @@ class WebhookClient
   end
 
   def chat
-    client = Slack::Web::Client.new
-    client.chat_postMessage(channel: 'D1GJCSMHC', text: "#{@event}", as_user: true)
-    client.chat_postMessage(channel: 'D1GJCSMHC', text: "lala", as_user: true)
+    @client = Slack::Web::Client.new
+    case @event
+    when "issue_comment"
+      issue_comment_message
+    when "pull_request_review_comment_message"
+      pull_request_review_comment_message
+    when "commit_comment_message"
+      commit_comment_message
+    else
+      unhandle_event_message
+    end
+  end
+
+  private
+
+  def issue_comment_message
+    @client.chat_postMessage(channel: 'D1GJCSMHC', text: "#{@event}", as_user: true)
+    @client.chat_postMessage(channel: 'D1GJCSMHC', text: "#{@payload[:comment][:body]}", as_user: true)
+  end
+
+  def pull_request_review_comment_message
+  end
+
+  def commit_comment_message
+  end
+
+  def unhandle_event_message
+    @client.chat_postMessage(channel: 'D1GJCSMHC', text: "#{@event}", as_user: true)
   end
 end
