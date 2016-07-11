e0680b3f7624dd0cfaee9f4ef93ac24ac7e3874f
peiyee <tehpeiyee_1989@hotmail.com>
2016-07-11 14:26:30 +0800

    updated gem

--- slack_github_observer/github_observer.rb
+++ slack_github_observer/github_observer.rb
@@ -3,8 +3,6 @@ require 'bot'
 
 Thread.abort_on_exception = true
 
-unless $0 =~ /rake$/
-  Thread.new do
-    SlackGithubObserver::Bot.run
-  end
+Thread.new do
+  SlackGithubObserver::Bot.run
 end
