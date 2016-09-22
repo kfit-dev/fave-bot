a52a2d7b68dcc3395c80cc78294e80554fa4463b
peiyee <tehpeiyee_1989@hotmail.com>
2016-09-22 11:31:57 +0800

    update method

--- slack_github_observer/commands/how_are_you.rb
+++ slack_github_observer/commands/how_are_you.rb
@@ -4,11 +4,7 @@ module SlackGithubObserver
       command 'how are you?'
 
       def self.call(client, data, match)
-        expression = match['expression']
-        if expression.blank?
-          client.say(channel: data.channel, text: "Currently I am working on Jira notification integration. I am trying to private message you if you are mentioned on any Jira Activity. Stay tuned~~~")
-          return
-        end
+        client.say(channel: data.channel, text: "Currently I am working on Jira notification integration. I am trying to private message you if you are mentioned on any Jira Activity. Stay tuned~~~")
       end
     end
   end
