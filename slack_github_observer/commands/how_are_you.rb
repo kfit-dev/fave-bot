8d3fe3c8160605484f1277ffc419fa6d1483ee23
peiyee <tehpeiyee_1989@hotmail.com>
2016-09-22 11:34:49 +0800

    updated message

--- slack_github_observer/commands/how_are_you.rb
+++ slack_github_observer/commands/how_are_you.rb
@@ -1,10 +1,10 @@
 module SlackGithubObserver
   module Commands
     class HowAreYou < SlackRubyBot::Commands::Base
-      command 'how are you?'
+      command 'how are you'
 
       def self.call(client, data, match)
-        client.say(channel: data.channel, text: "Currently I am working on Jira notification integration. I am trying to private message you if you are mentioned on any Jira Activity. Stay tuned~~~")
+        client.say(channel: data.channel, text: "hi <@#{data.user}>, i am working on Jira notification integration. I am trying to private message you if you are mentioned on any Jira Activity. Any feedback please talk to my master...")
       end
     end
   end
